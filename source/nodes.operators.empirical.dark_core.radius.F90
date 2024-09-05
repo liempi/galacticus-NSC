ce35884ea14f24fddf20d8969988f813ce595b28
Matías Liempi <71166666+liempi@users.noreply.github.com>
2024-09-05 12:32:04 -0400

    use solveAnalytics method for dark core radius node operator

--- source/nodes.operators.empirical.dark_core.radius.F90
+++ source/nodes.operators.empirical.dark_core.radius.F90
@@ -39,7 +39,7 @@
      private
      double precision :: efficiency       
    contains
-     procedure :: differentialEvolution => darkCoreRadiusDifferentialEvolution
+     procedure :: differentialEvolutionSolveAnalytics => darkCoreRadiusDifferentialEvolutionSolveAnalytics
   end type nodeOperatorDarkCoreRadius
   
   interface nodeOperatorDarkCoreRadius
@@ -91,69 +91,37 @@ contains
     return
   end function darkCoreRadiusConstructorInternal
   
-    subroutine darkCoreRadiusDifferentialEvolution(self,node,interrupt,functioninterrupt,propertyType)
+  subroutine darkCoreRadiusDifferentialEvolutionSolveAnalytics(self,node,time)
       !!{
         Compute the nuclear star cluster gas mass rate change.
       !!}
-    use :: Galacticus_Nodes , only : interruptTask           , nodeComponentNSC             , nodeComponentDarkCore, &
-                                 &   nodeComponentNSCStandard, nodeComponentDarkCoreStandard, propertyInactive     , & 
-                                 &   treeNode
+    use :: Galacticus_Nodes , only : nodeComponentNSC             , nodeComponentDarkCore, nodeComponentNSCStandard, &
+                &                    nodeComponentDarkCoreStandard, treeNode
     implicit none
-    class(nodeOperatorDarkCoreRadius), intent(inout), target :: self
-    type (treeNode                  ), intent(inout), target :: node
-    logical                          , intent(inout)         :: interrupt
-    procedure (interruptTask        ), intent(inout), pointer:: functioninterrupt
-    integer                          , intent(in   )         :: propertyType
+    class(nodeOperatorDarkCoreRadius), intent(inout)         :: self
+    type (treeNode                  ), intent(inout)         :: node
+    double precision                 , intent(in   )         :: time
     class (nodeComponentNSC         ),                pointer:: NSC
     class (nodeComponentDarkCore    ),                pointer:: darkCore
     double precision                                         :: radiusNSC, radiusDarkCore
-
-    ! Return immediately if inactive variables are requested.
-    if (propertyInactive(propertyType)) return
+    !$GLC attributes unused :: time
 
     NSC      =>  node%     NSC()
     darkCore =>  node%darkCore()
 
-    !Check if there is a NSC in the node.
-    select type (NSC)
-      type is  (nodeComponentNSC        )
-        ! Generic NSC component. Nothing to do here.
-        return
-      class is (nodeComponentNSCStandard)
-        ! Standard NSC class. Get the radius and check if it is positive.
-        radiusNSC = NSC%radius()
-        if (radiusNSC > 0.0d0) then
-          !Check if the Dark Core component has already initialized.
-          select type (darkCore)
-            type is (nodeComponentDarkCore         )
-            ! Null Dark Core class in this node, but there is a NSC class.
-            ! Let's interrupt the ODE solver and create a Dark Core standard  
-            interrupt = .true.
-            ! point to the interrupt method to create the Dark Core
-            functionInterrupt => DarkCoreCreate
-            return
-            class is (nodeComponentDarkCoreStandard)
-            ! Standard class, compute the dark core radius and set.
-              radiusDarkCore = self%efficiency * radiusNSC
-              call darkCore%radiusSet(radiusDarkCore)
-              return
-          end select
-        end if 
-    end select 
+    radiusNSC = NSC%radius()
+    if (radiusNSC > 0.0d0) then
+      !Check if the Dark Core component has already initialized.
+      select type (darkCore)
+        type is (nodeComponentDarkCore)
+          !Null Dark Core class. Do nothing.
+          return
+        class default 
+          !default class, compute the dark core radius and set.
+          radiusDarkCore = self%efficiency * radiusNSC
+          call darkCore%radiusSet(radiusDarkCore)
+          return
+      end select
+    end if 
     return
-  end subroutine darkCoreRadiusDifferentialEvolution
-
-  subroutine DarkCoreCreate(node,timeEnd)
-  !!{
-    Creates the Dark Core via interrupt.
-  !!}
-    use :: Galacticus_Nodes, only : interruptTask   , nodeComponentDarkCore, nodeComponentDarkCoreStandard, &
-          &                         propertyInactive, treeNode
-    implicit none
-    type (treeNode             ), intent(inout), target  :: node
-    double precision            , intent(in   ), optional:: timeEnd
-    class(nodeComponentDarkCore),                pointer :: darkCore
-    !$GLC attributes unused :: timeEnd
-    darkCore => node%darkCore(autoCreate=.true.)
-    return 
-  end subroutine DarkCoreCreate
+  end subroutine darkCoreRadiusDifferentialEvolutionSolveAnalytics
